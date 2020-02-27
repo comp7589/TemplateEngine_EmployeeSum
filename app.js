997152600a6ec6b962b6d89552dffcf924fd3dec
Ryan Compton <ryan.wade.compton@gmail.com>
2020-02-26 18:45:25 -0700

    Write libs repo code. Begin app inquiry.

--- app.js
+++ app.js
@@ -4,32 +4,81 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+const teamMembers = [];
+const id = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
+function initiateFunction() {
+    console.log("Build Your Squad.");
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "nameManager",
+            message: "What is your manager's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter valid name.";
+            }
+        },
+        {
+            type: "input",
+            name: "idManager",
+            message: "What is manager's ID?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "Please enter value greater than zero!"
+            }
+        },
+        {
+            type: "input",
+            name: "emailManager",
+            message: "What is manager's email?",
+            validate: answer => {
+                const pass = answer.match(
+                    /\S+@\S+\.\S+/
+                );
+                if(pass) {
+                    return true;
+                }
+                return "Please enter valid email address!"
+            }
+        },
+        ///Manger office number. DO like id above.
+    ])//.then starts here. 
+}
--- libs/Engineer.js
+++ libs/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class. HINT: This class is inherited from Employee.
+const Employee = require("./Employee")
+
+
+class Engineer extends Employee{
+    constructor(name, id, email, gitHub) {
+       super(name, id, email);
+        this.gitHub = gitHub;
+    }
+ 
+    getGitHub() {
+        return this.gitHub;
+    }
+    getRole() {
+        return "engineer";
+    }
+}
+
+module.exports = Engineer;
